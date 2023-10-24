9b9a58f12f0f708d4dcbc3438acb812a6347ba80
Ruphasa <rfrizqifauzan@gmail.com>
2023-10-24 10:34:35 +0700

    adding file sorting

---
+++ BubbleSortExample24.java
@@ -0,0 +1,17 @@
+public class BubbleSortExample24 {
+    public static void main(String[] args) {
+        int[] intData = {34, 18, 87, 72, 32, 54, 43};
+        int temp=0;
+        for (int i = 0; i < intData.length; i++) {
+            for (int j = 1; j < intData.length; j++) {
+                if (intData[j-1]>intData[j]) {
+                    temp=intData[j];
+                    intData[j]=intData[j-1];
+                    intData[j-1]=temp;
+                }
+            }
+        }for (int i = 0; i < intData.length; i++) {
+            System.out.println(intData[i]);
+        }
+    }
+}
