e46599d5777cb9d1125edff519c90c507d722c09
Ruphasa <rfrizqifauzan@gmail.com>
2023-10-24 10:13:21 +0700

    rata dari nilai lulus dan tidak

--- ArrayRataNilai24.java
+++ ArrayRataNilai24.java
@@ -3,14 +3,25 @@ public class ArrayRataNilai24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
-        double rata2, total=0;
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("masukan nilai mahasiswa ke-"+i+" : ");
+        int counterL=0,counterT=0,mahasiswa;
+        double rataL,rataT, totalT=0,totalL=0;
+        System.out.print("berapa banyak mahasiswa : ");
+        mahasiswa = sc.nextInt();
+        for (int i=0; i < mahasiswa; i++) {
+            System.out.print("masukan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
-        } for (int i = 0; i < nilaiMhs.length; i++) {
-            total+=nilaiMhs[i];
+        }for (int i=0; i < mahasiswa; i++) {
+            if (nilaiMhs[i]>70) {
+                totalL+=nilaiMhs[i];
+                counterL++;
+            }else{
+                totalT+=nilaiMhs[i];
+                counterT++;
         }
-        rata2=total/nilaiMhs.length;
-        System.out.println("rata-rata nilai = "+rata2);
+    }
+    rataL=totalL/counterL;
+    rataT=totalT/counterT;
+    System.out.println("rata-rata nilai mahasiswa lulus = "+rataL);
+    System.out.println("rata-rata nilai mahasiswa tidak lulus = "+rataT);
     }
 }
