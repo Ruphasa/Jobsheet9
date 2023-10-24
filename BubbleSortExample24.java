c83c1f329ef9a5ca77a6f1f24fb327df18cd8269
Ruphasa <rfrizqifauzan@gmail.com>
2023-10-24 10:39:41 +0700

    sorting descending

--- BubbleSortExample24.java
+++ BubbleSortExample24.java
@@ -4,7 +4,7 @@ public class BubbleSortExample24 {
         int temp=0;
         for (int i = 0; i < intData.length; i++) {
             for (int j = 1; j < intData.length; j++) {
-                if (intData[j-1]>intData[j]) {
+                if (intData[j-1]<intData[j]) {
                     temp=intData[j];
                     intData[j]=intData[j-1];
                     intData[j-1]=temp;
