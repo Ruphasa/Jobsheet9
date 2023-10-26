d90e2728bbc8fd2eb7120d2bce0f3b7b45404abc
Ruphasa <rfrizqifauzan@gmail.com>
2023-10-26 23:37:31 +0700

    pertanyaan search

--- LinearSearch24.java
+++ LinearSearch24.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
 public class LinearSearch24 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("berapa banyak elemen yang ada :");
+        int elemen = sc.nextInt();
+        int[] arrayInt=new int[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("elemen ke-"+(i+1)+" : ");
+            arrayInt[i]=sc.nextInt();
+        }
+        System.out.print("mau nyari elemen berapa : ");
+        int key=sc.nextInt();
+
+        int hasil = -1;
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
               hasil=i;
               break;  
-            } 
+            }
         }
+        if (hasil == -1) {
+            System.out.println("elemen tidak ditemukan");
+        }else
         System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
     }
 }
